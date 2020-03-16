1fd1a2e3f3cae72b605cf07c4d677c3c01e92c0e
Hernan Bernardo Quintero Jaramillo <bernardo.quintero.j@gmail.com>
2020-03-16 18:07:42 -0500

    Desarrollo realizado en blazor para la prueba tecnica

---
+++ Data/ElementsModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuestionTest.Data
+{
+    public class ElementsModel
+    {
+        public string category { get; set; }
+        public string type { get; set; }
+        public string difficulty { get; set; }
+        public string question { get; set; }
+        public string correct_answer { get; set; }
+        public List <string> incorrect_answers { get; set; }
+        public string id { get; set; }
+        
+    }
+}
---
+++ Data/MatrizModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuestionTest.Data
+{
+    public class MatrizModel
+    {
+        public string response_code { get; set; }
+        public List<ElementsModel> results { get; set; }
+    }
+}
